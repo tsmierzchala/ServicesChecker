05859f334a0ec430ff6e1e663baf38bf96d775ac
Shaqal7 <shaqal7@gmail.com>
2024-09-26 21:07:19 +0200

    added LocalServiceChecker class

---
+++ ServicesChecker/LocalServiceChecker.cs
@@ -0,0 +1,23 @@
+﻿using System.ServiceProcess;
+
+namespace ServicesChecker
+{
+    public class LocalServiceChecker
+    {
+        // Method to get the status of a specific service by name
+        public string CheckServiceStatus(string serviceName)
+        {
+            try
+            {
+                using (ServiceController serviceController = new ServiceController(serviceName))
+                {
+                    return $"Service {serviceName} is {serviceController.Status}";
+                }
+            }
+            catch (InvalidOperationException e)
+            {
+                return $"Error: {e.Message}";
+            }
+        }
+    }
+}
