83ffecb9355a124606bff76821c706d4eab3e529
Tomasz Śmierzchała <tsmierzchala@forcom.com.pl>
2024-09-27 10:21:54 +0200

    transform the URL into a more concise format

---
+++ ServicesChecker/UrlShortenerConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace ServicesChecker
+{
+    public class UrlShortenerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string url)
+            {
+                // Shorten URL to a maximum of 30 characters for display, you can customize this
+                int maxLength = 30;
+                if (url.Length > maxLength)
+                {
+                    return url.Substring(0, maxLength - 3) + "...";
+                }
+                return url;
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
